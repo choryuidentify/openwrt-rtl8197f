6dba5d7b4659b7d2d52ecf6cad2755240442c71d
Gaspare <gaspare@anlix.io>
2020-08-04 13:42:08 -0300

    Add Suport for Lexra RLX4181 LX4180 RLX5281

--- include/target.mk
+++ include/target.mk
@@ -172,6 +172,8 @@ ifeq ($(DUMP),1)
     CPU_CFLAGS_24kc = -mips32r2 -mtune=24kc
     CPU_CFLAGS_74kc = -mips32r2 -mtune=74kc
     CPU_CFLAGS_octeonplus = -march=octeon+ -mabi=64
+    CPU_CFLAGS_lx53 = -march=lx5380
+    CPU_CFLAGS_lx43 = -march=lx4380
   endif
   ifeq ($(ARCH),i386)
     CPU_TYPE ?= pentium-mmx
